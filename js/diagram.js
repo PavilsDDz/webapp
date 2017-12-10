da45635330f87cae3774386e0fe2ef3b43143f72
PavilsDDz <pavilsdzi@gmail.com>
2017-12-11 00:50:37 +0200

    some more stuff done

--- js/diagram.js
+++ js/diagram.js
@@ -1,59 +1,38 @@
-<script type="text/javascript" src="js/jquery.js"></script>
-<div style="position: relative;">
-<canvas style="border: solid" width="500" height="500" id="canvas1">
-	
-</canvas>
-<div class="par active" id="par1"></div>
-<div class="par " id="par2"></div>
-<div class="par " id="par3"></div>
-</div>
-<div class="income"></div>
-<div class="expenses"></div>
 
-<button id="next" onmousedown="caulculate_dif()">next</button>
-<button id="prev">brev</button>
-<style type="text/css">
-	.par {
-		width: 10px;
-		height: 10px;
-		position: absolute;
-		background-color: red;
-		top: 0;
-		left: 0;
-	}
-</style>
-
-<script type="text/javascript">
 	var months
 	var values_to 
 	var values
 	var old_rot = 0
 	var csl = 0.4 	//circle radius large
 	var csm = 0.37 	//circle radius medium
-	var css = 0.3 	//circle radius small
+	var css = 0.23 	//circle radius small
 	var r1, r2;
 	$.getJSON("data.json", function(json) {
 	    // console.log(json); // this will show the info it in firebug console
 	    months = json
-		values = months[1]//['january']
+		values = months[0]//['january']
 		console.log(values)
 		draw_diag(values['income'],values['expenses'],values['pars'])
+
+		for (var i = 0; i < months.length; i++) {
+			$('.time_line').append('<div class="month"><p>'+months[i]['month']+'</p></div>')
+		}
 	});
 		Ww = $(window).width()
 		canvas = document.getElementById('canvas1')
-		cw = canvas.width = Ww<500 ? Ww : 500
-		ch = canvas.height = Ww<500 ? Ww : 500
+		cw = canvas.width = Ww*0.85<500 ? Ww*0.85 : 500
+		ch = canvas.height = Ww*0.85<500 ? Ww*0.85 : 500
 		ctx = canvas.getContext('2d')
 	 	ctx.translate(cw/2,cw/2)
 		ctx.rotate(210*Math.PI/180)
 
 		var grd=ctx.createRadialGradient(0,0,cw*0.45,0,0,0);
 		grd.addColorStop(0,"#FF2366");
-		grd.addColorStop(0.3,"#8D4DE8");
+		grd.addColorStop(0.5,"#8D4DE8");
 
 		var grd2=ctx.createRadialGradient(0,0,cw*0.45,0,0,0);
 		grd2.addColorStop(0,"#FD3F2F");
-		grd2.addColorStop(0.3,"#FACE15");
+		grd2.addColorStop(0.5,"#FACE15");
 
 	var now_deg	= {}
 	var pars_now = []
@@ -123,8 +102,10 @@
 		ctx.arc(0, 0, cw*css, 0, 2 * Math.PI);
 
 
-		ctx.fillStyle = "#ffffff"
+		ctx.fillStyle = "#161823"
 		ctx.fill()
+
+		ctx.rotate(-old_rot)
 
 		$('.income').html(values['income'])
 		$('.expenses').html(values['expenses'])
@@ -139,8 +120,8 @@
 	dif_pars = [0,0,0]
 	now_pars = [0,0,0]
 	dif_pars = [0,0,0]
-	function caulculate_dif(){
-		values_to = months[4]
+	function caulculate_dif(to){
+		values_to = months[to]
 
 		dif.a = values_to['income']
 		dif.b = values_to['expenses']
@@ -152,22 +133,21 @@
 		now.b = values['expenses']
 		now.c = Math.abs(now.a-now.b)
 
-		dif_deg.push(now_deg.a - (dif.a*dif.step)*Math.PI/180)
-		dif_deg.push(now_deg.c - (dif.c*dif.step)*Math.PI/180)
-		dif_deg.push(now_deg.b - (dif.b*dif.step)*Math.PI/180)
+		dif_deg[0]=(now_deg.a - (dif.a*dif.step)*Math.PI/180)
+		dif_deg[1]=(now_deg.c - (dif.c*dif.step)*Math.PI/180)
+		dif_deg[2]=(now_deg.b - (dif.b*dif.step)*Math.PI/180)
 
-		dif_pars.push(
-			pars_now[0]-values_to['pars'][0]*dif.step*Math.PI/180,
-			pars_now[1]-values_to['pars'][1]*dif.step*Math.PI/180,
-			pars_now[2]-values_to['pars'][2]*dif.step*Math.PI/180
-		)
+		dif_pars[0] = pars_now[0]-values_to['pars'][0]*dif.step*Math.PI/180,
+		dif_pars[1] = pars_now[1]-values_to['pars'][1]*dif.step*Math.PI/180,
+		dif_pars[2] = pars_now[2]-values_to['pars'][2]*dif.step*Math.PI/180
+		
 
 
 		console.log(now_deg.a , (dif.a*dif.step)*Math.PI/180)
 
 
 		
-		ctx.rotate(-old_rot)
+		//old_rot = 0;
 		animate_diag()
 
 		$('.income').html(values_to['income'])
@@ -238,38 +218,54 @@
 		ctx.arc(0, 0, cw*css, 0, 2 * Math.PI);
 
 
-		ctx.fillStyle = "#ffffff"
+		ctx.fillStyle = "#161823"
 		ctx.fill()
 		
 		place_divs()
 		
 		//console.log(progress)
 		if (progress<1) {
+			//console.log(progress)
 			requestAnimationFrame(animate_diag)
+		}else{
+			now_deg.a = now_deg.a-dif_deg[0]
+			now_deg.c = now_deg.c-dif_deg[1]
+			now_deg.b = now_deg.b-dif_deg[2]
+
+			pars_now[0] = pars_now[0]-dif_pars[0]
+			pars_now[1] = pars_now[1]-dif_pars[1]
+			pars_now[2] = pars_now[2]-dif_pars[2]
+
+			progress = 0
 		}
 	}
 	function place_divs(){
 		$('#par1').css({
 			'left':
-			Math.sin(pars_now[0]+dif_pars[0]*progress+210*Math.PI/180)*(cw/2+cw/2)*csl+cw/2,
+			Math.sin(pars_now[0]+dif_pars[0]*progress+210*Math.PI/180)*(cw/2+cw/2)*csl+cw/2-10,
 			'top':
-			Math.cos(pars_now[0]+dif_pars[0]*progress+210*Math.PI/180)*(cw/2+cw/2)*csl+cw/2})
+			Math.cos(pars_now[0]+dif_pars[0]*progress+210*Math.PI/180)*(cw/2+cw/2)*csl+cw/2-10})
 
 		$('#par3').css({
 			'left':
-			Math.sin(pars_now[1]-dif_pars[1]*progress+(r1)-60*Math.PI/180)*(cw/2+cw/2)*csm+cw/2,
+			Math.sin(pars_now[1]-dif_pars[1]*progress+(r1)-60*Math.PI/180)*(cw/2+cw/2)*csm+cw/2-10,
 			'top':
-			-Math.cos(pars_now[1]-dif_pars[1]*progress+(r1)-60*Math.PI/180)*(cw/2+cw/2)*csm+cw/2})
+			-Math.cos(pars_now[1]-dif_pars[1]*progress+(r1)-60*Math.PI/180)*(cw/2+cw/2)*csm+cw/2-10})
 
 		$('#par2').css({
 			'left':
-			Math.sin(pars_now[2]-dif_pars[2]*progress+(r1+r2)-60*Math.PI/180)*(cw/2+cw/2)*csl+cw/2,
+			Math.sin(pars_now[2]-dif_pars[2]*progress+(r1+r2)-60*Math.PI/180)*(cw/2+cw/2)*csl+cw/2-10,
 			'top':
-			-Math.cos(pars_now[2]-dif_pars[2]*progress+(r1+r2)-60*Math.PI/180)*(cw/2+cw/2)*csl+cw/2})
+			-Math.cos(pars_now[2]-dif_pars[2]*progress+(r1+r2)-60*Math.PI/180)*(cw/2+cw/2)*csl+cw/2-10})
 
 	}
 	$(window).resize(function(){
 		cw = canvas.width = Ww<500 ? Ww : 500
 		ch = canvas.height = Ww<500 ? Ww : 500
 	})
-</script>
+
+	$('.time_line').delegate('.month','click',function(){
+	
+			caulculate_dif($(this).index())
+
+	})

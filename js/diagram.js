18ac062ec24605b45c0765b208e8761e618b7fac
PavilsDDz <pavilsdzi@gmail.com>
2017-12-18 00:30:50 +0200

    Mostly done, just little css left and tow bugs in diagram.js

--- js/diagram.js
+++ js/diagram.js
@@ -11,32 +11,112 @@
 	    // console.log(json); // this will show the info it in firebug console
 	    months = json
 		values = months[0]//['january']
-		console.log(values)
+		//console.log(values)
+		//caulculate_dif(0)
 		draw_diag(values['income'],values['expenses'],values['pars'])
 
 		for (var i = 0; i < months.length; i++) {
-			$('.time_line').append('<div class="month"><p>'+months[i]['month']+'</p></div>')
+			if (i==0) {
+				$('.time_line').append('<div class="month active"><p>'+months[i]['month']+'</p><div class="line"><div></div>')
+			}else{
+				$('.time_line').append('<div class="month"><p>'+months[i]['month']+'</p><div class="line"><div></div>')
+			}
+			
 		}
+
+
+
+		inc_a = values['income']+'';
+		inc_b = inc_a.split('.')
+		
+		exp_a = values['expenses']+''
+		exp_b = exp_a.split('.')
+
+		$('.income').html('<h3>$+'+inc_b[0]+'<span>'+inc_b[1]+'</span></h3>')
+		$('.expenses').html('<h3>$-'+exp_b[0]+'<span>'+exp_b[1]+'</span></h3>')
+		
+		
+		
+		let bal_raw = values['income'] - values['expenses']// + ''
+		
+		let bal_a = bal_raw.toFixed(2)+''
+		let bal_b = bal_a.split('.')
+		//console.log(bal_b)
+		
+		$('.balance h3').html('$'+bal_b[0]+'<span>'+bal_b[1]+'</span>')
+
+
+
+
 	});
+	var Ww
+	var Wh
+	var cw
+	var ch
+
+	$(function(){
 		Ww = $(window).width()
+		Wh = $(window).height()
 		canvas = document.getElementById('canvas1')
-		cw = canvas.width = Ww*0.85<500 ? Ww*0.85 : 500
-		ch = canvas.height = Ww*0.85<500 ? Ww*0.85 : 500
+		ch = canvas.height = cw = canvas.width = Ww*0.85/Wh < 0.6 ? Ww*0.85 : Wh*0.6
+		$('.diagarm').css({'height':ch,'width':cw})
 		ctx = canvas.getContext('2d')
-	 	ctx.translate(cw/2,cw/2)
-		ctx.rotate(210*Math.PI/180)
+		$('.balance').css({'width':cw*css*2,'height':ch*css*2})
 
-		var grd=ctx.createRadialGradient(0,0,cw*0.45,0,0,0);
-		grd.addColorStop(0,"#FF2366");
-		grd.addColorStop(0.5,"#8D4DE8");
 
-		var grd2=ctx.createRadialGradient(0,0,cw*0.45,0,0,0);
-		grd2.addColorStop(0,"#FD3F2F");
-		grd2.addColorStop(0.5,"#FACE15");
+		 if (Ww*0.85/Wh > 0.6) {
+		 	// $('.diagarm .dwrap').css('left',(Ww-cw)/2)
+		 }
+
+
+
+		 /*
+			PAR CLICK FUNCTION
+		 */
+
+		 $('.par').click(function(){
+		 	let id = $(this).index()
+		 	//console.log(id)
+		 	// $('.par.active').animate({ backgroundSize: '30%' }, 300)
+		 	$(this).animate({ backgroundSize: '150%' }, {duration:300,step:function(a){$('.par.active').css('background-size',150-(a*0.8)+'%')},complete:function(){$('.par.active').removeClass('active');$(this).addClass('active')}});
+
+		 	$('.info_par.active').animate({'opacity':0},150,function(){
+		 		//console.log($('.info_par:eq('+(id-1)+')'))
+		 		$(this).removeClass('active')
+		 		$('.info_par:eq('+(id-1)+')').addClass('active')
+		 		$('.info_par.active').animate({'opacity':1},150)
+		 	})
+		 })
+
+
+
+			
+		})
+
+
+	 	//ctx.translate(cw/2,cw/2)
+		//ctx.rotate(210*Math.PI/180)
+
+		var grd
+		var grd2
+		
 
 	var now_deg	= {}
 	var pars_now = []
 	function draw_diag(a,b,pars){
+		old_rot = 0
+
+		grd=ctx.createRadialGradient(0,0,cw*0.45,0,0,0);
+		grd.addColorStop(0,"#FF2366");
+		grd.addColorStop(0.5,"#8D4DE8");
+
+		grd2=ctx.createRadialGradient(0,0,cw*0.45,0,0,0);
+		grd2.addColorStop(0,"#FD3F2F");
+		grd2.addColorStop(0.5,"#FACE15");
+
+	 	ctx.translate(cw/2,cw/2)
+		ctx.rotate(210*Math.PI/180)
+
 
 		total = a+b+Math.abs(a-b)
 		let dif = Math.abs(a-b)
@@ -107,12 +187,12 @@
 
 		ctx.rotate(-old_rot)
 
-		$('.income').html(values['income'])
-		$('.expenses').html(values['expenses'])
-		console.log(now_deg.a,now_deg.c,now_deg.b)
+		//$('.income').html(values['income'])
+		//$('.expenses').html(values['expenses'])
+		//console.log(now_deg.a,now_deg.c,now_deg.b)
 		place_divs()
 	}
-	speed = 0.01
+	speed = 0.03
 	progress = 0
 	dif = {}
 	now = {}
@@ -143,16 +223,35 @@
 		
 
 
-		console.log(now_deg.a , (dif.a*dif.step)*Math.PI/180)
+		//console.log(now_deg.c , (dif.c*dif.step)*Math.PI/180)
 
 
 		
 		//old_rot = 0;
 		animate_diag()
 
-		$('.income').html(values_to['income'])
-		$('.expenses').html(values_to['expenses'])
-		console.log(dif_deg)
+		inc_a = dif.a+'';
+		inc_b = inc_a.split('.')
+
+		exp_a = dif.b+''
+		exp_b = exp_a.split('.')
+
+		$('.income').html('<h3>$+'+inc_b[0]+'<span>'+inc_b[1]+'</span></h3>')
+		$('.expenses').html('<h3>$-'+exp_b[0]+'<span>'+exp_b[1]+'</span></h3>')
+		
+		
+		
+		let bal_raw = dif.a - dif.b// + ''
+		
+		let bal_a = bal_raw.toFixed(2)+''
+		let bal_b = bal_a.split('.')
+		//console.log(bal_b)
+		
+		$('.balance h3').html('$'+bal_b[0]+'<span>'+bal_b[1]+'</span>')
+		
+		console.log(pars_now[0],dif_pars[0])
+
+
 	}
 	progress_curve = 0
 	function animate_diag(){
@@ -210,7 +309,6 @@
 		ctx.fillStyle = 'rgba(0,0,0,0.2)'
 		ctx.fill()
 
-		ctx.rotate(-old_rot)
 
 		ctx.beginPath()
 
@@ -238,34 +336,64 @@
 
 			progress = 0
 		}
+		ctx.rotate(-old_rot)
 	}
+
+
 	function place_divs(){
+		//console.log(pars_now[0],dif_pars[0],progress+210*Math.PI/180)
 		$('#par1').css({
 			'left':
-			Math.sin(pars_now[0]+dif_pars[0]*progress+210*Math.PI/180)*(cw/2+cw/2)*csl+cw/2-10,
+			Math.sin(pars_now[0]+dif_pars[0]*progress+210*Math.PI/180)*(cw/2+cw/2)*csl+cw/2-(15+Ww/100)/2,
 			'top':
-			Math.cos(pars_now[0]+dif_pars[0]*progress+210*Math.PI/180)*(cw/2+cw/2)*csl+cw/2-10})
+			Math.cos(pars_now[0]+dif_pars[0]*progress+210*Math.PI/180)*(cw/2+cw/2)*csl+cw/2-(15+Ww/100)/2})
 
 		$('#par3').css({
 			'left':
-			Math.sin(pars_now[1]-dif_pars[1]*progress+(r1)-60*Math.PI/180)*(cw/2+cw/2)*csm+cw/2-10,
+			Math.sin(pars_now[1]-dif_pars[1]*progress+(r1)-60*Math.PI/180)*(cw/2+cw/2)*csm+cw/2-(15+Ww/100)/2,
 			'top':
-			-Math.cos(pars_now[1]-dif_pars[1]*progress+(r1)-60*Math.PI/180)*(cw/2+cw/2)*csm+cw/2-10})
+			-Math.cos(pars_now[1]-dif_pars[1]*progress+(r1)-60*Math.PI/180)*(cw/2+cw/2)*csm+cw/2-(15+Ww/100)/2})
 
 		$('#par2').css({
 			'left':
-			Math.sin(pars_now[2]-dif_pars[2]*progress+(r1+r2)-60*Math.PI/180)*(cw/2+cw/2)*csl+cw/2-10,
+			Math.sin(pars_now[2]-dif_pars[2]*progress+(r1+r2)-60*Math.PI/180)*(cw/2+cw/2)*csl+cw/2-(15+Ww/100)/2,
 			'top':
-			-Math.cos(pars_now[2]-dif_pars[2]*progress+(r1+r2)-60*Math.PI/180)*(cw/2+cw/2)*csl+cw/2-10})
+			-Math.cos(pars_now[2]-dif_pars[2]*progress+(r1+r2)-60*Math.PI/180)*(cw/2+cw/2)*csl+cw/2-(15+Ww/100)/2})
 
 	}
+
+
+
 	$(window).resize(function(){
-		cw = canvas.width = Ww<500 ? Ww : 500
-		ch = canvas.height = Ww<500 ? Ww : 500
+		Ww=$(window).width()
+		Wh=$(window).height()
+		//cw < 500 || cw > ch*0.3 ? ah = aw = cw
+		 ch = canvas.height = cw = canvas.width = Ww*0.85/Wh < 0.6 ? Ww*0.85 : Wh*0.6
+		 if (Ww/Wh > 0.6) {
+		 	// $('.diagarm .dwrap').css('left',(Ww*0.85-cw)/2)
+		}
+		$('.diagarm').css({'height':ch,'width':cw})
+		//ch = canvas.height = Ww<500 ? Ww : 500
+		draw_diag(months[0]['income'],months[0]['expenses'],months[0]['pars'])
+		$('.balance').css({'width':cw*css*2,'height':ch*css*2})
+	
 	})
 
 	$('.time_line').delegate('.month','click',function(){
-	
+			
 			caulculate_dif($(this).index())
+			$(this).children('.line').animate({'opacity':1},{
+				duration: 300,
+				step: function(o){
+					$('.month.active .line').css({'opacity':1-o})
+					//console.log($('.month.active .line'))
+				},
+				complete:function(){
+					$('.month.active').removeClass('active')
+					$(this).parent().addClass('active')
+				}
+
+			})
 
 	})
+
